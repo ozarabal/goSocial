ad8594314c966fdd9dfd20f130570789be39858b
ozarabal <r.ardhinto@gmail.com>
2025-01-30 11:50:43 +0700

    feat: error handling

---
+++ cmd/api/errors.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"net/http"
+)
+
+func (app *application) internalServerError(w http.ResponseWriter, r *http.Request, err error) {
+	
+	app.logger.Errorw("interal server error", "method", r.Method, "path", r.URL.Path, "error", err.Error() )
+
+	writeJSONError(w, http.StatusInternalServerError, "the server encountered a problem")
+}
+
+func (app *application) badRequestResponse(w http.ResponseWriter, r *http.Request, err error) {
+	
+	app.logger.Warnf("Bad request error", "method", r.Method, "path", r.URL.Path, "error", err.Error() )
+
+	writeJSONError(w, http.StatusBadRequest, "the server encountered a problem")
+}
+
+func (app *application) conflictResponse(w http.ResponseWriter, r *http.Request, err error) {
+	
+	app.logger.Errorw("Confict respose error", "method", r.Method, "path", r.URL.Path, "error", err.Error() )
+
+	writeJSONError(w, http.StatusConflict, "the server encountered a problem")
+}
+
+func (app *application) notFoundResponse(w http.ResponseWriter, r *http.Request, err error) {
+	
+	app.logger.Warnf("not found error", "method", r.Method, "path", r.URL.Path, "error", err.Error() )
+
+	writeJSONError(w, http.StatusNotFound, "the server encountered a problem")
+}
