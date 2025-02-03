89298fa29bf9a991760da544784c32e5459913bf
ozarabal <r.ardhinto@gmail.com>
2025-02-03 17:41:21 +0700

    fix : unused middleware

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -140,35 +140,6 @@ func (app *application) unfollowUserHandler(w http.ResponseWriter, r *http.Reque
 	}
 }
 
-func (app *application) userContextMiddleware(next http.Handler) http.Handler{
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
-		userID, err := strconv.ParseInt(chi.URLParam(r, "userID"), 10, 64)
-		if err != nil {
-			app.badRequestResponse(w, r, err)
-			return
-		}
-
-		ctx := r.Context()
-
-		user, err :=  app.store.Users.GetUserByID(ctx, userID)
-		if err != nil{
-			switch err {
-			case store.ErrorNotFound:
-				app.notFoundResponse(w,r,err)
-				return 
-			default:
-				app.internalServerError(w,r,err)
-				return
-			}
-		}
-
-		ctx = context.WithValue(ctx, userCtx, user)
-
-		next.ServeHTTP(w, r.WithContext(ctx))
-
-	})
-}
-
 func getUserFromContext(r *http.Request) *store.User {
 	user,_ := r.Context().Value(userCtx).(*store.User)
 	return user
